c07adba1b6749e432e061f317bd9fe28688cf26b
Joao Guimaraes <jssgmrs22@gmail.com>
2020-02-17 00:10:12 +0000

    Starting "Projects" section (making the fun parts)

---
+++ components/projects.js
@@ -0,0 +1,17 @@
+const Projects = () => {
+  return (
+    <div className="projects">
+      <h1 className="projects-title subtitle">Projects</h1>
+      <div className="projects-container projects-middle">
+        <div className="projects-container-proj projects-container-proj-1"></div>
+        <div className="projects-container-proj projects-container-proj-2"></div>
+        <div className="projects-container-proj projects-container-proj-3"></div>
+        <div className="projects-container-proj projects-container-proj-4"></div>
+        <div className="projects-container-proj projects-container-proj-5"></div>
+        <div className="projects-container-proj projects-container-proj-6"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Landing from "../components/landing";
 import "../css/index.css";
+import Projects from "../components/projects";
 
 const Home = () => (
   <div>
@@ -14,7 +15,7 @@ const Home = () => (
       ></link>
     </Head>
     <Landing />
-    <h1>Hello World</h1>
+    <Projects />
   </div>
 );
 
